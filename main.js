0b99de9c4acb074dd1053f5f25c9869fc20b5fb7
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-07 10:10:52 -0600

    added employee functions

--- main.js
+++ main.js
@@ -3,4 +3,27 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('example.sqlite');  // our connection to the sqlite database
 
-console.log(db)
+const dropEmployees = () => {   // funtion to clear table
+  db.run(`DROP TABLE employees`);
+}
+// dropEmployees()
+
+const populateEmployees () => {
+
+
+}
+
+db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary NUMBER(7, 2))");
+
+
+
+
+
+
+// db.run("INSERT INTO employees (id, first, last, salary) VALUES (1, 'Michael', 'Scott', 60000)");
+// db.run("INSERT INTO employees VALUES (2, 'Jim', 'Halpert', 45000)");
+// db.run(`INSERT INTO employees VALUES (3, "Billy", "Conolly", 100000)`)
+// employees TABLE
+// id |  first    |   last    | salary
+//  1 | 'Michael' | 'Scott'   |  60k
+//  2 | 'Jim'     | 'Halpert' |  45k
